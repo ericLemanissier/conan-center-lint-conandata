543183a446bfcfd61ba092b4f5c2fd6a51a62987
ericLemanissier <ericLemanissier@users.noreply.github.com>
2025-12-26 12:30:11 +0100

    process cocorepo

--- run_lint.sh
+++ run_lint.sh
@@ -6,7 +6,7 @@ then
     res=$(python3 $(dirname "$0")/lint_conandata.py ${conandata})
     if [[ -n "$res" ]]
     then
-        echo "## [${d}](https://github.com/conan-io/conan-center-index/tree/master/recipes/${conandata})"
+        echo "## [${d}](https://github.com/ericLemanissier/cocorepo/tree/master/recipes/${conandata})"
         echo "${res}"
         echo ""
     fi
